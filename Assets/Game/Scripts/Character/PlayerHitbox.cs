85007e8b957a083503fc249f9d08171a9a209c08
Kh4n <aikilm69@gmail.com>
2024-06-05 10:22:23 +0700

    feat: add impact zone for enemy and player

---
+++ Assets/Game/Scripts/Character/PlayerHitbox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHitbox : MonoBehaviour
+{
+	[SerializeField] private float radius = 1f;
+	[SerializeField] private LayerMask enemyLayer; // Enemy's layer
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+
+	private void Update()
+	{
+		Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, radius, enemyLayer);
+		foreach (Collider2D enemy in hitEnemies)
+		{
+			Debug.Log("Player tấn công Enemy");
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Enemy/EnemyHitbox.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+	[SerializeField] private float radius = 1f;
+	[SerializeField] private LayerMask playerLayer; // Player layer
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+
+	private void Update()
+	{
+		Collider2D[] playerHit = Physics2D.OverlapCircleAll(transform.position, radius, playerLayer);
+		foreach (Collider2D enemy in playerHit)
+		{
+			Debug.Log("Player mất máu");
+		}
+	}
+}
