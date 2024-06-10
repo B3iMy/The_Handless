37c689e4f3d5be3b4e8ccd70ddfb66c3ca2303b0
FUROFUSHI <nguyenchianh2342003@gmail.com>
2024-06-10 11:25:36 +0700

    fix:fix Playerbehavior

---
+++ Assets/Game/Scripts/Enemy/EnemyHitbox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hitbox : MonoBehaviour
+{
+	[SerializeField] private float radius = 1f;
+	[SerializeField] private LayerMask playerLayer; // Player layer
+
+	[SerializeField] private float attackDelay = 0.5f;
+
+	public PlayerBehaviour playerHit;
+	public ScriptableEntity entity;
+
+	[SerializeField] private float attackCooldown = 0f;
+
+	private void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, radius);
+	}
+
+	private void Update()
+	{
+		attackCooldown -= Time.deltaTime; // Giảm bộ đếm thời gian theo thời gian thực
+
+		if (attackCooldown <= 0f) // Chỉ tấn công khi bộ đếm thời gian bằng hoặc nhỏ hơn 0
+		{
+			Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(transform.position, radius, playerLayer);
+			foreach (Collider2D enemy in hitEnemies)
+			{
+				playerHit = enemy.GetComponent<PlayerBehaviour>();
+
+				if (playerHit != null)
+				{
+					playerHit.TakeHit(entity.atk);
+					attackCooldown = attackDelay; // Đặt lại bộ đếm thời gian
+					Debug.Log("Player tấn công Enemy");
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Game/Scripts/Thành/Behaviour/PlayerBehaviour.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    public ScriptableEntity entity;
+
+    public float Hitpoints;
+    public float MaxHitpoints;
+    public HealthBarBehaviour Healthbar;
+    void Start()
+    {
+        MaxHitpoints = entity.hp;
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+        // Add SphereCollider if not already present
+        //SphereCollider collider = gameObject.GetComponent<SphereCollider>();
+        //if (collider == null)
+        //{
+        //    collider = gameObject.AddComponent<SphereCollider>();
+        //}
+
+        //collider.isTrigger = true; // Set as trigger if you want to use OnTriggerEnter instead of OnCollisionEnter
+    }
+    public void TakeHit(float damage)
+    {
+        Hitpoints -= damage;
+
+        if (Hitpoints <= 0)
+        {
+            Destroy(gameObject);
+        }
+        // Update the health bar
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+        Healthbar.gameObject.SetActive(true);
+    }
+}
