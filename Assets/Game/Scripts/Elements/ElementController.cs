6c80c0be6b2ad2dec6268f15550c1f8a9754a43e
FUROFUSHI <nguyenchianh2342003@gmail.com>
2024-06-14 14:50:11 +0700

    feat: indicator not update

--- Assets/Game/Scripts/Elements/ElementController.cs
+++ Assets/Game/Scripts/Elements/ElementController.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using System.Xml.Linq;
 using UnityEngine;
@@ -19,17 +19,48 @@ public abstract class ElementController : MonoBehaviour
 	protected float lastAttackTime;
 	protected float lastSkillTime;
 
-	protected virtual void Start()
+    // indicator initial field
+    [SerializeField] protected Vector2 abilityPos;
+    [SerializeField] protected Canvas abilityCanvas;
+    [SerializeField] protected Image abilityCircleImg;
+    [SerializeField] protected Transform playerTranform;
+
+    protected Vector2 position;
+    protected RaycastHit2D hit;
+    protected Ray2D ray;
+    protected bool isSkillButtonHeld = false;
+
+    protected virtual void Start()
 	{
 		attackButton.onClick.AddListener(PerformNormalAttack);
 		skillButton.onClick.AddListener(PerformSkill);
+		abilityCircleImg.enabled = false;
+		abilityCanvas.enabled = false;
 	}
 
 	private void Update()
 	{
-	}
+        if (isSkillButtonHeld)
+        {
+            UpdateSkillIndicator();
+
+            // Kiểm tra khi người chơi nhả nút chuột
+            if (Input.GetMouseButtonUp(0))
+            {
+                ExecuteSkillAtPosition(abilityPos);
+                isSkillButtonHeld = false;
+                abilityCircleImg.enabled = false;
+            }
+        }
+    }
 
 	protected abstract void PerformNormalAttack();
 
 	protected abstract void PerformSkill();
+	protected abstract void UpdateSkillIndicator();
+	protected abstract void ExecuteSkillAtPosition(Vector2 position);
+
+	protected abstract void onDisable();
+	protected abstract void Onenable();
+	protected abstract void OnSkillButtonDown();
 }
--- Assets/Game/Scripts/Elements/Fire/FireElementController.cs
+++ Assets/Game/Scripts/Elements/Fire/FireElementController.cs
@@ -29,7 +29,8 @@ public class FireElementController : ElementController
 
 	protected override void PerformSkill()
 	{
-		if (Time.time - lastSkillTime > skillCooldown)
+		
+        if (Time.time - lastSkillTime > skillCooldown)
 		{
 			if (currentElement != null && currentElement.skillPrefab != null)
 			{
@@ -52,4 +53,77 @@ public class FireElementController : ElementController
 			}
 		}
 	}
+	protected override void UpdateSkillIndicator()
+	{
+		// Lấy vị trí của người chơi
+		position = playerTranform.position;
+
+		// Tạo một tia từ vị trí của người chơi về phía chuột
+		Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		Vector2 direction = mousePosition - position;
+		ray = new Ray2D(position, direction);
+
+		// Thực hiện raycast trong không gian 2D
+		hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+		// Tính toán vị trí của skill indicator
+		if (hit.collider != null)
+		{
+			// Nếu tia va chạm với một đối tượng, đặt skill indicator tại điểm va chạm
+			abilityPos = hit.point;
+		}
+		else
+		{
+			// Nếu không có va chạm, đặt skill indicator tại điểm cuối của tia (ví dụ: khoảng cách tối đa)
+			abilityPos = ray.origin + ray.direction * 10.0f; // 10.0f là khoảng cách tối đa
+		}
+
+		// Cập nhật vị trí của hình ảnh skill indicator trong không gian màn hình
+		Vector2 screenPoint = Camera.main.WorldToScreenPoint(abilityPos);
+		abilityCircleImg.transform.position = screenPoint;
+
+		// Hiển thị skill indicator
+		abilityCircleImg.enabled = true;
+	}
+    protected override void ExecuteSkillAtPosition(Vector2 position)
+    {
+        if (Time.time - lastSkillTime > skillCooldown)
+        {
+            if (currentElement != null && currentElement.skillPrefab != null)
+            {
+                GameObject skill = Instantiate(currentElement.skillPrefab, position, Quaternion.identity);
+
+                // Add FireWallUnit component to each child of the skill (FireWall)
+                FireWallUnit[] units = skill.GetComponentsInChildren<FireWallUnit>();
+                foreach (var unit in units)
+                {
+                    unit.damage = currentElement.skillDamage;
+                }
+
+                Destroy(skill, currentElement.skillDuration);
+                lastSkillTime = Time.time;
+            }
+            else
+            {
+                Debug.LogError("Skill prefab or element is null");
+            }
+        }
+    }
+
+    protected override void Onenable()
+    {
+        skillButton.onClick.AddListener(OnSkillButtonDown);
+    }
+    protected override void onDisable()
+    {
+        skillButton.onClick.RemoveListener(OnSkillButtonDown);
+
+    }
+
+    protected override void OnSkillButtonDown()
+    {
+        isSkillButtonHeld = true;
+
+    }
+
 }
