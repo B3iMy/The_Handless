98d3262a89a48a62cc40b92c1af7118a85640fcd
FUROFUSHI <nguyenchianh2342003@gmail.com>
2024-06-17 16:06:41 +0700

    feat: spinner indicator

--- Assets/Game/Scripts/Elements/Fire/FireElementController.cs
+++ Assets/Game/Scripts/Elements/Fire/FireElementController.cs
@@ -34,19 +34,29 @@ public class FireElementController : ElementController
 
     protected override void Ability2Canvas(Vector3 worldPosition)
     {
-        int layerMask = ~LayerMask.GetMask("Player");
+        //int layerMask = ~LayerMask.GetMask("Player");
 
-        hit = Physics2D.Raycast((Vector2)playerTransform.position, ((Vector2)worldPosition - (Vector2)playerTransform.position).normalized, maxAbilityDistance, layerMask);
-        if (hit.collider != null && hit.collider.gameObject != this.gameObject)
-        {
-            position = hit.point;
+        //hit = Physics2D.Raycast((Vector2)playerTransform.position, ((Vector2)worldPosition - (Vector2)playerTransform.position).normalized, maxAbilityDistance, layerMask);
+        //if (hit.collider != null && hit.collider.gameObject != this.gameObject)
+        //{
+        //    position = hit.point;
 
-            Vector2 hitPosDir = ((Vector2)hit.point - (Vector2)playerTransform.position).normalized;
-            float distance = Vector2.Distance(hit.point, playerTransform.position);
-            distance = Mathf.Min(distance, maxAbilityDistance);
-            Vector2 newHitpoint = (Vector2)playerTransform.position + hitPosDir * distance;
-            abilityCanvas.transform.position = new Vector3(newHitpoint.x, newHitpoint.y, abilityCanvas.transform.position.z);
-        }
+        //    Vector2 hitPosDir = ((Vector2)hit.point - (Vector2)playerTransform.position).normalized;
+        //    float distance = Vector2.Distance(hit.point, playerTransform.position);
+        //    distance = Mathf.Min(distance, maxAbilityDistance);
+        //    Vector2 newHitpoint = (Vector2)playerTransform.position + hitPosDir * distance;
+        //    abilityCanvas.transform.position = new Vector3(newHitpoint.x, newHitpoint.y, abilityCanvas.transform.position.z);
+        //}
+        // Calculate direction and distance directly from playerTransform to the worldPosition
+        Vector2 direction = ((Vector2)worldPosition - (Vector2)playerTransform.position).normalized;
+        float distance = Vector2.Distance(worldPosition, playerTransform.position);
+        distance = Mathf.Min(distance, maxAbilityDistance);
+
+        // Calculate the new hit point based on direction and clamped distance
+        Vector2 newHitpoint = (Vector2)playerTransform.position + direction * distance;
+
+        // Update the position of the abilityCanvas
+        abilityCanvas.transform.position = new Vector3(newHitpoint.x, newHitpoint.y, abilityCanvas.transform.position.z);
     }
     protected override void ActivateSkill(Vector3 position)
     {
